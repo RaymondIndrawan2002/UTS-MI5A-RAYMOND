67f5bef60568bdb4270a3bc3f54d1558d52d08a2
RaymondIndrawan2002 <raymondindrawansilalahi_2225110002@mhs.mdp.ac.id>
2024-10-30 14:54:30 +0700

    UTS_MI5A_Raymond
    UTS_MI5A_Raymond

---
+++ categories-app/app/Http/Controllers/categoriesController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\cr;
+use Illuminate\Http\Request;
+
+class categoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(cr $cr)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(cr $cr)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, cr $cr)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(cr $cr)
+    {
+        //
+    }
+
+     public function getcategories(){
+        $response['data'] = categories::all();
+        $response['message'] = 'List data categories';
+        $response['success'] = true;
+
+        return response()->json($response, 200);
+
+    }
+
+}
